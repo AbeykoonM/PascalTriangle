ba42425a9428036487edb9206c902c7234747cd6
AbeykoonM <mahesha.1edu@gmail.com>
2024-05-03 11:12:28 +0300

    Add project files.

---
+++ main.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "screen.h"
+#include "pascal.h"
+
+int main(void){
+	int rows;
+	printf("Pascal Triangle\n");
+	printf("Enter the no of rows: ");
+	scanf("%d", &rows);
+	
+	clearScreen();
+	pascal_triangle(rows);
+	
+}
---
+++ screen.h
@@ -0,0 +1,8 @@
+#pragma once
+//define alll the colors as constant
+enum COLORS {BLACK=30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
+
+//FUNCTION DEClARATION
+void setFGcolor(int);
+void clearScreen(void);
+void gotoXY(int, int);
